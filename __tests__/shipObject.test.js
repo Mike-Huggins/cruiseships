96bf585a168f0546bbe2a3006664e069fc55186e
Mike H <mikeyh1001@hotmail.com>
2019-03-14 20:16:58 +0000

    started again with the ship constructor

--- __tests__/shipObject.test.js
+++ __tests__/shipObject.test.js
@@ -1,66 +1,16 @@
+/* globals describe it expect */
 const { Ship } = require('../src/shipObject.js');
 const { Port } = require('../src/portObject.js');
 
-describe('create a new instance of ship', () => {
-  let megacruise;
-  let southampton;
-  let portsmouth;
+describe('Ship constructor test suite', () => {
+  let ship;
   beforeEach(() => {
-    megacruise = new Ship('Megacruise', southampton, 100, 0);
-    southampton = new Port('Southampton');
-    portsmouth = new Port('Portsmouth');
+    ship = new Ship('Dover');
   });
-
-  xit('creates a new ship instance', () => {
+  it('creates a new ship instance', () => {
     expect(new Ship()).toBeInstanceOf(Object);
   });
-
-  xit('check port imported from other module correctly', () => {
-    expect(new Port()).toBeInstanceOf(Object);
-  });
-
-  xit('check port is brought into ship correctly', () => {
-    expect(megacruise.port).toEqual(southampton);
-  });
-
-  xit('check port details', () => {
-    expect(megacruise.port.name).toEqual('Southampton');
-  });
-
-  xit('ensure all parameters populate the ship object', () => {
-    expect(megacruise.name).toEqual('Megacruise');
-    expect(megacruise.passengerCap).toEqual(100);
-    expect(megacruise.passengers).toEqual(0);
-  });
-
-  xit('add and subtract passengers and have a check for remaining places', () => {
-    megacruise.passengerAdd(20);
-    expect(megacruise.passengers).toEqual(20);
-    megacruise.passengerSubtract(15);
-    expect(megacruise.passengers).toEqual(5);
-    megacruise.passengerSubtract(5);
-    expect(megacruise.passengers).toEqual(0);
-    expect(() => megacruise.passengerAdd(101)).toThrow('That is 1 too many, draw straws!');
-    expect(() => megacruise.passengerSubtract(5)).toThrow('That is 5 more people leaving than expected, were there stowaways?!?')
-  });
-
-  xit('make the ship set sail and not be in port', () => {
-    megacruise.toggleSail();
-    expect(megacruise.inPort).toEqual(false);
-    expect(megacruise.shipCheck()).toEqual('You are on the high seas!');
-    megacruise.toggleSail();
-    expect(megacruise.inPort).toEqual(true);
-    expect(megacruise.shipCheck()).toEqual('You are in dock');
-    megacruise.toggleSail();
-    expect(megacruise.inPort).toEqual(false);
-    expect(megacruise.shipCheck()).toEqual('You are on the high seas!');
-  });
-  xit('create a dock method to change port within ship', () => {
-    megacruise.dock(portsmouth);
-    expect(megacruise.port).toEqual(portsmouth);
-  });
-  xit('test name of new port', () => {
-    megacruise.dock(portsmouth);
-    expect(megacruise.port.name).toEqual('Portsmouth');
+  it('create a new ship with starting port of Dover', () => {
+    expect(ship.startingPort).toEqual('Dover');
   });
 });
--- src/shipObject.js
+++ src/shipObject.js
@@ -1,33 +1,11 @@
-const startingPassengers = 0;
-const startInPort = true;
 const { Port } = require('../src/portObject.js');
 
-function Ship(name, port, passengerCap, passengers) {
-  this.name = name;
-  this.port = port;
-  this.passengerCap = passengerCap;
-  this.passengers = startingPassengers;
-  this.inPort = startInPort;
+function Ship(startingPort) {
+  this.startingPort = startingPort;
 };
 
 // eslint-disable-next-line func-names
-Ship.prototype.passengerAdd = function (value) {
-  if ((this.passengers + value) > this.passengerCap) {
-    throw new Error(`That is ${((this.passengers + value) - this.passengerCap)} too many, draw straws!`);
-  }
-  this.passengers += value;
-};
-
-// eslint-disable-next-line func-names
-Ship.prototype.passengerSubtract = function (value) {
-  if ((this.passengers - value) < 0) {
-    throw new Error(`That is ${-(this.passengers - value)} more people leaving than expected, were there stowaways?!?`);
-  }
-  this.passengers -= value;
-};
-
-// eslint-disable-next-line func-names
-Ship.prototype.toggleSail = function () {
+/* Ship.prototype.toggleSail = function () {
   this.inPort = !this.inPort;
 };
 
@@ -44,7 +22,7 @@ Ship.prototype.shipCheck = function () {
 Ship.prototype.dock = function (newPort) {
   this.port = newPort;
 };
-
+*/
 module.exports = {
   Ship,
-};
+}; 
