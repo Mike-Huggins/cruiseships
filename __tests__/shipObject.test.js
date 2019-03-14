c0da585a6fcee8c55fcdb7e4888b1e99dbed7f36
Mike H <mikeyh1001@hotmail.com>
2019-03-14 20:57:36 +0000

    add and remove ship from the port

--- __tests__/shipObject.test.js
+++ __tests__/shipObject.test.js
@@ -1,15 +1,20 @@
 /* globals describe it expect */
 const { Ship } = require('../src/shipObject.js');
 const { Port } = require('../src/portObject.js');
+const { Itenerary } = require('../src/itinerary.js');
 
 describe('Ship constructor test suite', () => {
   let ship;
   let southampton;
   let calais;
+  let itenerary;
+  let ship2;
   beforeEach(() => {
     ship = new Ship(southampton);
     southampton = new Port('Southampton');
     calais = new Port('Calais');
+    itenerary = new Itenerary([southampton]);
+    ship2 = new Ship(itenerary);
   });
   xit('creates a new ship instance', () => {
     expect(new Ship()).toBeInstanceOf(Object);
@@ -24,5 +29,8 @@ describe('Ship constructor test suite', () => {
   xit('Ship docks at a different port', () => {
     ship.dock(calais);
     expect(ship.currentPort).toEqual(calais);
+  });
+  it('port added to ship itenerary on ship instantiation', () => {
+    expect(southampton.ships).toContain(ship2);
   });
 });
